9ac96c97e81858f5e9556db95f83bc3739ff567b
Xien <xien2530@gmail.com>
2026-04-03 00:27:32 -0500

    Upgrade Test 5

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json({ limit: '15mb' }));
 app.use(express.urlencoded({ extended: true, limit: '15mb' }));
 app.use(express.static(path.join(__dirname)));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.use(cookieSession({
   name:     'bt_session',
